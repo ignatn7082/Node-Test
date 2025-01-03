82367f30558815f3ea6d509a81de54849e08d208
ignatn7082 <nttanhn@gmail.com>
2025-01-03 22:41:21 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 const routes = [
     {
-        path: "/",
-        name: "contactbook",
-        component: ContactBook,
-    },
+            path: "/:pathMatch(.*)*", // Khớp với bất kỳ đường dẫn nào không tìm thấy
+            name: "notfound",
+            component: NotFound, // Thành phần hiển thị trang Not Found
+          },
 ];
 const router = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL),
