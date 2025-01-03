d864445f74b644043ae3c2b81839df7882150b70
ignatn7082 <nttanhn@gmail.com>
2025-01-03 22:55:30 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,21 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 const routes = [
     {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+        },
+    {
             path: "/:pathMatch(.*)*", // Khớp với bất kỳ đường dẫn nào không tìm thấy
             name: "notfound",
             component: NotFound, // Thành phần hiển thị trang Not Found
-          },
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+        },
 ];
 const router = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL),
