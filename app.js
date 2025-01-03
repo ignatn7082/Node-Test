3832d5e805dcb853becc779a07b15d6ea54c1398
ignatn7082 <nttanhn@gmail.com>
2025-01-03 19:11:43 +0700

    cai dat xu li loi

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ const app = express();
 
 const contactsRouter = require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
+
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+
+app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Not found"));
+});
+
+//define error-handing middleware last
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });        
+});
 
 app.get("/",(req, res) => {
     res.json({ message: "Welcome to contact book applocation."});
